3ece66eae7228c0041a1d5431fc2f32641e32ded
mohammed <zchizzz11111@gmail.com>
2026-02-05 17:27:57 +0200

    Update Companies component layout for better mobile responsiveness and enhance text visibility

--- app/components/Companies.jsx
+++ app/components/Companies.jsx
@@ -20,7 +20,8 @@ const Company = () => {
         <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
           شركات التأمين المتعاقدة معنا <Image src="/images/companies.png" alt="" width={42} height={42} className="inline-block w-14 h-14 mb-1" />
         </h2>
-        <div className="hidden md:grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-8">
+        {/* mobile grid */}
+        <div className="md:hidden grid  grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-8">
               <div
                 
                 className="group flex-1/2 bg-gray-50 border border-gray-200 rounded-2xl p-8 hover:border-[#3bb7dc] hover:shadow-xl transition duration-300 flex flex-col items-center justify-center"
@@ -55,7 +56,7 @@ const Company = () => {
                 />
                   
                 </div>
-                <h3 className="text-sm text-center md:text-xl py-3">الشركة المتحدة للتأمين</h3>
+                <h3 className="text-sm text-center text-black md:text-xl py-3">الشركة المتحدة للتأمين</h3>
               </div>
               <div
                 
@@ -70,7 +71,7 @@ const Company = () => {
                   alt="Insurance Logo"
                   className="object-cover w-full h-full rounded-2xl"
                 />
-                <h3 className="text-sm md:text-xl pt-2">شركة كهرباء السودان القابضة المحدودة</h3>
+                <h3 className="text-sm md:text-xl text-black pt-2">شركة كهرباء السودان القابضة المحدودة</h3>
               </div>
               <div
                 
@@ -85,7 +86,7 @@ const Company = () => {
                   alt="Insurance Logo"
                   className="object-cover w-full h-full rounded-2xl"
                 />
-                <h3 className="text-sm md:text-xl pt-2">ديوان الذكاة</h3>
+                <h3 className="text-sm md:text-xl text-black pt-2">ديوان الذكاة</h3>
               </div>
               <div
                 
@@ -103,14 +104,14 @@ const Company = () => {
                   />
 
                 </div>
-                <h3 className="text-sm text-center md:text-xl py-3">البركة للتأمين</h3>
+                <h3 className="text-sm text-center text-black md:text-xl py-3">البركة للتأمين</h3>
               </div>
         </div>
-        {/* Desktop Grid */}
-        <motion.div initial={{y: 20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{delay: 0.3, duration: 0.5}} className="md:hidden flex flex-col  text-black gap-6 mt-4">
+
+        {/* Desktop mo Grid */}
+        <motion.div initial={{y: 20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{delay: 0.3, duration: 0.5}} className="hidden md:grid flex-col  text-black gap-6 mt-4">
           <div className="flex gap-3">
               <div
-                
                 className="group flex-1/2 bg-gray-50 border border-gray-200 rounded-2xl p-8 hover:border-[#3bb7dc] hover:shadow-xl transition duration-300 flex flex-col items-center justify-center"
               >
                 <div className="">
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
+
+// module.exports = nextConfig;
 
 export default nextConfig;
